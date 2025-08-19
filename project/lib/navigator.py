5b54857c4de44d06c1ec8253fd5db67103fd4918
Lucazhouzhou <lucas.zhou610@education.nsw.gov.au>
2025-08-19 01:55:30 +0000

    eng

--- project/lib/navigator.py
+++ project/lib/navigator.py
@@ -2,9 +2,10 @@ from PiicoDev_Unified import sleep_ms
 from colour import Colour
 from ultrasonic import Ultrasonic
 from wheels import Wheels
+from OLED import OLED
 
 class Navigator():
-    def __init__(self,forwardfast,forwardslow,turnright,turnleft,stop,wallincoming,rightsidehaswall,isgreen,debug):
+    def __init__(self,forwardfast,forwardslow,turnright,turnleft,stop,wallincoming,rightsidehaswall,isgreen,show_state,clear,debug):
         self.__forwardfast = forwardfast
         self.__forwardslow = forwardslow
         self.__turnright  = turnright
@@ -14,11 +15,21 @@ class Navigator():
         self.__rightsidehaswall = rightsidehaswall
         self.__isgreen = isgreen
         self.__debug = debug
+        self.__showstate = show_state
+        self.__clear = clear
+        self.state = "IDLE"
 
     def controller(self):
         while True:
+            self.__showstate(self.state)
+            if self.__isgreen():
+                self.state = "GREEN"
+                self.__showstate(self.state)
+                self.__stop()
+                sleep_ms(500)
             if self.__wallincoming():
-                    print(self.__wallincoming)
+                    self.state = "FACEDWALL"
+                    self.__showstate(self.state)
                     self.__stop()
                     sleep_ms(200)
                     self.__turnleft()
