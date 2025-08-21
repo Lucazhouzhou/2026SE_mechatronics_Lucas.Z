39326c2228f71315a9e065e065f7436834b2ca51
Lucazhouzhou <lucas.zhou610@education.nsw.gov.au>
2025-08-21 17:03:46 +0000

    navi change

--- project/lib/navigator.py
+++ project/lib/navigator.py
@@ -37,19 +37,11 @@ class Navigator():
                     self.__turnleft()
                     sleep_ms(519)
             elif not self.__wallincoming() and not self.__rightsidehaswall() :
-<<<<<<< HEAD
-                    self.__forwardfast()
-                    sleep_ms(500)
-                    self.__stop()
-                    sleep_ms(200)
-                    self.state = "TURNING RIGHT"
-=======
                     self.state = "FACEDWALL"
                     self.__showstate(self.state)
                     self.__stop()
                     sleep_ms(200)
                     self.state = "TURNINGLEFT"
->>>>>>> a04b3cc (latenight)
                     self.__showstate(self.state)
                     self.__turnright()
                     sleep_ms(519)
